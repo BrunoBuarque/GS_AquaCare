0cb98fbf02866731bdb51d0568b3e3742be51669
BrunoBuarque <RM550597@fiap.com.br>
2024-06-07 11:04:04 -0300

    Toques Finais JS

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ document.addEventListener("DOMContentLoaded", function() {
     var toggleButton = document.querySelector("#toggle-button");
     var clarificationSection = document.querySelector("#clarification");
 
+    /* Ocultar Padrão */
     clarificationSection.style.display = "none";
 
+    /* Centralizar e Margem */
+    toggleButton.style.display = "block";
+    toggleButton.style.margin = "30px auto";
+
+    /* Evento de Clique */
     toggleButton.addEventListener("click", function() {
         if (clarificationSection.style.display === "none") {
             clarificationSection.style.display = "block";
-            toggleButton.textContent = "Ocultar";
+            toggleButton.textContent = "Ocultar!";
         } else {
             clarificationSection.style.display = "none";
-            toggleButton.textContent = "Exibir";
+            toggleButton.textContent = "Saiba Mais!";
         }
     });
 });
